f4a442eaff9a9f3cbb9c34f97d2945f9ab730a62
Sky-General <SGeneral109@gmail.com>
2014-02-16 13:31:28 -0800

    Second Commit

--- src/me/skyGeneral/signs/Main.java
+++ src/me/skyGeneral/signs/Main.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.plugin.PluginManager;
@@ -14,22 +15,18 @@ public class Main extends JavaPlugin implements Listener {
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(this, this);
 	}
+	@EventHandler
 	public void onPlayerPlaceSign(SignChangeEvent e){
-		if(e.getPlayer().hasPermission("mw.ranksign")){
-			if(e.getLine(0) == "[ranksign]"){
-				Player target = Bukkit.getServer().getPlayer(e.getLine(2));
-				String l0 = e.getLine(0);
-				String l1 = e.getLine(1);
-				String l2 = e.getLine(2);
-				String l3 = e.getLine(3);
-				e.setLine(0, l1);
-				e.setLine(1, l2);
-				if(target.isOnline()){
-					e.setLine(2, ChatColor.GREEN + "Online");
-				}else{
-					e.setLine(2, ChatColor.RED + "Offline");
+		Player player = (Player) Bukkit.getOfflinePlayer(e.getLine(3));
+		if(player.hasPlayedBefore()){
+			if(player.isOnline()){
+				Bukkit.broadcastMessage(ChatColor.GREEN + "Online!");
 				}
+			else{
+				Bukkit.broadcastMessage(ChatColor.RED + "Offline");
 			}
+		
+			
 		}
 	}
 }
